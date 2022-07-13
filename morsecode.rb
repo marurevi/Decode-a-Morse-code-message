2a53c037218f3a46121a4ac7d2e6df538c6cfbdf
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-07-13 18:16:36 -0300

    Add method decode_word

--- morsecode.rb
+++ morsecode.rb
@@ -25,8 +25,16 @@ LETTERS = {
   '-..-' => 'X',
   '-.--' => 'Y',
   '--..' => 'Z'
-}
+}.freeze
 
 def decode_char(char)
   LETTERS[char]
 end
+
+def decode_word(word)
+  my_array = word.split(/ /, 2)
+  my_array.each { |i| print decode_char(i) }
+end
+
+puts decode_char '--..'
+decode_word '-- -.--'
